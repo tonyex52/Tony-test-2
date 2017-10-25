7a8ee93ad9458f8ebb61b91b9ea7be23da111395
Zhongyang Wang 王仲揚 <tonyex52@corp.ruten.com.tw>
2017-10-25 18:24:21 +0800

    [mod] add computedWannaSearch array

--- src/components/main.vue
+++ src/components/main.vue
@@ -3,13 +3,13 @@
     <div class="section">
       <div class="section-title">Word Creation</div>
       <div class="section-content">
-        <textarea class="type-area" v-default-value="123" v-model="wordCreation" placeholder="type single|multiple words which wanna be searched (make sure that you use the LF between words)"></textarea>
+        <textarea class="type-area" v-default-value="'123'" v-model="wordCreation" placeholder="type single|multiple words which wanna be searched (make sure that you use the LF between words)"></textarea>
       </div>
     </div>
     <div class="section">
-      <div class="section-title">Wanna Search</div>
+      <div class="section-title">Wanna Search <label class="label-text"><input type="checkbox" name="case-sensitive" v-model="isCaseSensitive"> Case Sensitive</label></div>
       <div class="section-content">
-        <textarea class="type-area" v-default-value="123" v-model="wannaSearch" placeholder="type single|multiple words you wanna search (make sure that you use the LF between words)"></textarea>
+        <textarea class="type-area" v-default-value="'123\n456\n789'" v-model="wannaSearch" placeholder="type single|multiple words you wanna search (make sure that you use the LF between words)"></textarea>
       </div>
     </div>
   </div>
@@ -19,17 +19,27 @@
 import Vue from 'vue'
 Vue.use(require('../plugins/vue-default-value'))
 
+const SPLIT_CHARACTER = '\n'
+
 export default {
   name: 'main',
   data () {
     return {
       wordCreation: '',
-      wannaSearch: ''
+      wannaSearch: '',
+      isCaseSensitive: false
     }
   },
   computed: {
-    computedwordCreation () {
-      return this.wordCreation.indexOf('\n') >= 0 ? this.wordCreation.split('\n').filter((value) => value.length) : [this.wordCreation]
+    computedWordCreation () {
+      return this.wordCreation.indexOf(SPLIT_CHARACTER) >= 0 ? this.wordCreation.split(SPLIT_CHARACTER).filter((value) => value.length) : [this.wordCreation]
+    },
+    computedWannaSearch () {
+      let wannaSearchArray = this.wannaSearch.indexOf(SPLIT_CHARACTER) >= 0 ? this.wannaSearch.split(SPLIT_CHARACTER).filter((value) => value.length) : [this.wannaSearch]
+
+      return wannaSearchArray.map((value) => {
+        return value.split('')
+      })
     }
   }
 }
